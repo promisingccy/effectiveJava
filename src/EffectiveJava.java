9fdccb16118c10a0c0265c2bfe78d958c2f97c60
chengyang.chen <chengyang.chen@tophant.com>
2020-12-04 10:59:48 +0800

    test

--- src/EffectiveJava.java
+++ src/EffectiveJava.java
@@ -13,6 +13,7 @@ public class EffectiveJava {
         // MultiConstructParam obj = new MultiConstructParam.Builder(1, 2)
         //     .c(3).d(4).e(5).f(6).build();
         // System.out.println(obj);
+
         
     }
 
